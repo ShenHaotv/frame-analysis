8698aba321780f8f9dd6a749b56f7405f354dd05
ShenHaotv <129112332+ShenHaotv@users.noreply.github.com>
2025-03-06 10:20:20 -0600

    Update steppe_code.py

--- Empirical Data/3500-1500 BCE aDNA/steppe_code.py
+++ Empirical Data/3500-1500 BCE aDNA/steppe_code.py
@@ -6,7 +6,7 @@ from frame.visualization import Vis
 from frame.cross_validation import run_cv
 from frame.digraphstats import Digraphstats
 
-"""To get the genotype file, please download the v62.0_1240k_public dataset from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/FFIDCW, apply the filters we showed in the supplementary, delete the repeating samples (for this step, we suggest using the steppe_individual_list.csv), and do the mean value imputation """
+"""To get the genotype file, please download the v62.0_1240k_public dataset from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/FFIDCW, apply the filters we showed in the supplementary, delete the repeating samples (for this step, we suggest using steppe_individual_list.csv), and do the mean value imputation """
 
 """For the coord, grid, edges file, please use steppe.coord, for the steppe_grid.csv and steppe_edges.csv """
 
