68da09661baec852278bbc29cd69cc4dff34a30a
ShenHaotv <129112332+ShenHaotv@users.noreply.github.com>
2025-03-06 10:11:31 -0600

    Update aDNA_steppe.py

--- Empirical Data/3500-1500 BCE aDNA/aDNA_steppe.py
+++ Empirical Data/3500-1500 BCE aDNA/aDNA_steppe.py
@@ -9,7 +9,7 @@ from frame.digraphstats import Digraphstats
 
 """To get the genotype file, please download the v62.0_1240k_public dataset from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/FFIDCW, apply the filters we showed in the supplementary, delete the repeating samples (for this step, we suggest using the individual_list_steppe.csv), and do the mean value imputation """
 
-"""For the coord, grid, edges file, please use steppe.coord, for the grid_path, """
+"""For the coord, grid, edges file, please use steppe.coord, for the grid_steppe.csv and edges_steppe.csv """
 
 sp_digraph = SpatialDiGraph(genotypes, coord, grid, edges)
 
