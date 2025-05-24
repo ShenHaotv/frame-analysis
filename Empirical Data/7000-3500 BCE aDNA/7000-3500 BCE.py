1ebae812a03feb35868899719375901de7e9a2ec
ShenHaotv <129112332+ShenHaotv@users.noreply.github.com>
2025-05-23 21:05:11 -0500

    Update 7000-3500 BCE.py

--- Empirical Data/7000-3500 BCE aDNA/7000-3500 BCE.py
+++ Empirical Data/7000-3500 BCE aDNA/7000-3500 BCE.py
@@ -6,7 +6,7 @@ from frame.visualization import Vis
 from frame.cross_validation import run_cv
 from frame.digraphstats import Digraphstats
 
-"""To get the genotype file, please download the v62.0_1240k_public dataset from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/FFIDCW, apply the filters we showed in the supplementary, delete the repeating samples (for this step, we suggest using individual_list_neolithic.csv), and do the mean value imputation """
+"""To get the genotype file, please download the v62.0_1240k_public dataset from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/FFIDCW, apply the filters we showed in the supplementary, delete the repeating samples (for this step, we suggest using 7000-3500 BCE_individual_list.csv), and do the mean value imputation """
 
 """For the coord, grid, edges file, please use 7000-3500 BCE.coord, 7000-3500 BCE_grid.csv and 7000-3500 BCE_edges.csv"""
 
